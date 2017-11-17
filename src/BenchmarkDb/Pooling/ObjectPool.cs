5d18b8e4f328b8e432002f39e4d1db3237166e75
Sebastien Ros <sebastienros@gmail.com>
2017-11-17 15:05:24 -0800

    Using ConcurrentQueue

--- src/BenchmarkDb/Pooling/ObjectPool.cs
+++ src/BenchmarkDb/Pooling/ObjectPool.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 
 namespace BenchmarkDb.Pooling
 {
     public class ObjectPool<T> where T : class
     {
+        private readonly ConcurrentQueue<T> _queue;
         private readonly T[] _items;
+        private readonly int _maximumRetained;
         private readonly Func<T> _factory;
 
         public ObjectPool(int maximumRetained, Func<T> factory)
@@ -16,32 +19,47 @@ namespace BenchmarkDb.Pooling
             }
 
             _items = new T[maximumRetained];
+            _queue = new ConcurrentQueue<T>();
+            _maximumRetained = maximumRetained;
             _factory = factory;
         }
 
         public T Get()
         {
-            for (var i = 0; i < _items.Length; i++)
+            if (!_queue.TryDequeue(out var item))
             {
-                var item = _items[i];
-                if (item != null && Interlocked.CompareExchange(ref _items[i], null, item) == item)
-                {
-                    return item;
-                }
+                item = _factory();
             }
 
-            return _factory();
+            return item;
+
+            //for (var i = 0; i < _items.Length; i++)
+            //{
+            //    var item = _items[i];
+            //    if (item != null && Interlocked.CompareExchange(ref _items[i], null, item) == item)
+            //    {
+            //        return item;
+            //    }
+            //}
+
+            //return _factory();
         }
 
         public bool Return(T obj)
         {
-            for (var i = 0; i < _items.Length; i++)
+            if (_queue.Count < _maximumRetained)
             {
-                if (Interlocked.CompareExchange(ref _items[i], obj, null) == null)
-                {
-                    return true;
-                }
+                _queue.Enqueue(obj);
+                return true;
             }
+
+            //for (var i = 0; i < _items.Length; i++)
+            //{
+            //    if (Interlocked.CompareExchange(ref _items[i], obj, null) == null)
+            //    {
+            //        return true;
+            //    }
+            //}
 
             return false;
         }
