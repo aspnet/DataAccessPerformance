a6bc898672b8d59f6c152477f8488b4a3baa356d
Jiri Cincura <jiri@cincura.net>
2018-06-15 10:19:07 +0200

    Added FirebirdClient.

---
+++ src/BenchmarkDb/FirebirdClientAdoDriver.cs
@@ -0,0 +1,25 @@
+﻿// Author: Jiri Cincura (jiri@cincura.net)
+
+using System;
+using System.Data.Common;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace BenchmarkDb
+{
+	public sealed class FirebirdClientAdoDriver : AdoDriver
+	{
+		public FirebirdClientAdoDriver()
+			: base(FirebirdClientFactory.Instance)
+		{ }
+
+		public override void Initialize(string connectionString, int _)
+		{
+			var settings = new FbConnectionStringBuilder(connectionString);
+
+			if (settings.Enlist)
+				throw new ArgumentException($"Enlist=false must be specified for {nameof(FirebirdSql.Data.FirebirdClient)}.");
+
+			base.Initialize(connectionString, _);
+		}
+	}
+}
